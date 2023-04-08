b69c59ab97de4c47d2d530262016c828b232e95b
samirasingh19 <samira.singh021@gmail.com>
2023-04-08 09:29:58 +0530

    LineComparision NonStatic

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LineComparisionNonStatic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Computation Problems");
+            //LineDiffrence Difference = new LineDiffrence();
+            //Difference.LineDistance();
+
+            //CheckLineEquality Equality = new CheckLineEquality();
+            //Equality.LineEquality();
+
+            //CompareTwoLines twoLines = new CompareTwoLines();
+            //twoLines.CompareLines();
+        }
+    }
+}
